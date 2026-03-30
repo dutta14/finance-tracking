76bb383e62c271110922096ece8f26fd5d1972b3
Anindya Dutta <anindya.dutta22@gmail.com>
2026-03-29 17:28:42 -0700

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/finance-tracking/', // Set to your repo name
   plugins: [react()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
